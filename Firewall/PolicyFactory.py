bbfdf7a912458aa44505f55f7b69ffb2c212056a
Milad Naderpour <milad.naderpour@gmail.com>
2022-04-05 10:21:57 +0430

    firewall policies

--- Firewall/PolicyFactory.py
+++ Firewall/PolicyFactory.py
@@ -1,20 +1,55 @@
-from FirewallPolicy import FirewallPolicy as fp
+from re import Match
+from typing import List
+from Firewall.FirewallPolicy import FirewallPolicy as Policy
+import re
 
 
 class PolicyFactory:
-    policy: fp = None
-    state: int
+    policies: List[Policy] = []
+    policy: Policy = None
+    state: int = 0
+    edit_re = re.compile('edit\\s(?P<policy_id>\\d{1,4})')
+    set_re = re.compile('set\\s(?P<parameter>\\w+)\\s(?P<value>\\S+)')
 
-    def New(self, ruleid: str) -> bool:
+    def create_new_policy(self, result: Match) -> bool:
+        p = Policy(result.group('policy_id'))
+        self.policy = p
+        self.state = 1
+        return True
+
+    def set_policy_parameter(self, result: Match) -> bool:
+        self.policy.set(result.group('parameter'), result.group('value'))
+        return True
+
+    def new(self, line: str) -> bool:
         if self.state != 0:
             return False
-        else:
-            p = fp(ruleid)
-            self.policy = p
-            self.state = 1
+        result = self.edit_re.search(line)
+        if result is not None:
+            return self.create_new_policy(result)
+        return True
 
-    def Set(self, parameter, value) -> bool:
+    def set(self, line) -> bool:
         if self.state != 1:
             return False
-        else:
-            self.policy.set(parameter, value)
+        result = self.set_re.search(line)
+        if result is not None:
+            return self.set_policy_parameter(result)
+        return False
+
+    def check_config_line(self, line: str) -> bool:
+        print(line)
+        if self.state == 0:
+            return self.new(line)
+        elif line == 'next':
+            self.policies.append(self.policy)
+            self.state = 0
+            return True
+        elif line == 'end':
+            self.state = 0
+            return False
+        return self.set(line)
+
+    def get_policies(self, lines):
+        for line in lines:
+            self.check_config_line(str(line).strip())
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from paramiko import SSHClient
 from paramiko.client import AutoAddPolicy
-import re
+from Firewall.PolicyFactory import PolicyFactory as PolicyFactory
 
 client = SSHClient()
 client.set_missing_host_key_policy(AutoAddPolicy())
 client.connect('10.1.3.89', username='Monitor', password='F!re$1$hc0O$um')
-ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("show firewall policy 2609")
+ssh_stdin, ssh_stdout, ssh_stderr = client.exec_command("show firewall policy")
 output = ssh_stdout.readlines()
 client.close()
 
-cnfRegex = re.compile("edit\\s(?P<id>\\d{1,4})")
-r = cnfRegex.search("    edit 2609")
-print(r)
-for line in output:
-    r = cnfRegex.search(line)
-    if r is not None:
-        print(line.strip())
-        print(r.group('id'))
+pf = PolicyFactory()
+pf.get_policies(output)
+for policy in pf.policies:
+    print(f'{policy.id} {policy.name} : Src= {policy.srcif} ({policy.srcaddr}) -> Dst={policy.dstif} ({policy.dstaddr}) | {policy.service} | {policy.action}')
+# cnfRegex = re.compile("edit\\s(?P<id>\\d{1,4})")
+# r = cnfRegex.search("    edit 2609")
+# print(r)
+# for line in output:
+#     r = cnfRegex.search(line)
+#     print(line.strip())
+#     if r is not None:
+#         print(r.group('id'))
